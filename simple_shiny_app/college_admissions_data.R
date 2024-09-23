afa0ba22c08086e1e06ce8d12ef78fd3e0833701
stevenwong15 <stevenwong15@gmail.com>
2024-09-23 06:29:51 -0700

    simple shiny app

---
+++ simple_shiny_app/college_admissions_data.R
@@ -0,0 +1,41 @@
+library(data.table)
+library(ggplot)
+library(shiny)
+
+DT <- fread("l.csv")[, .(
+	college_name = name,
+	parent_income_percentile = factor(
+		par_income_bin,
+		levels = par_income_bin,
+		labels = par_income_lab),
+	relative_attendance_rate = rel_attend
+	)]
+college_names <- unique(DT$college_name)
+
+ui <- pageWithSidebar(
+  headerPanel('College Attendance Rate by Family Income'),
+  sidebarPanel(
+    selectInput('college_name', 'College Name', college_names)
+  ),
+  mainPanel(
+    plotOutput('plot')
+  )
+)
+
+server <- function(input, output) {
+	output$plot <- renderPlot({
+		DT[college_name == input$college_name] %>%
+			ggplot(aes(
+				x = parent_income_percentile, 
+				y = relative_attendance_rate, 
+				group = 1)) +
+			geom_line() + 
+			geom_point() + 
+			expand_limits(y = 0) + 
+			labs(
+				x = "Parent Income Percentile",
+				y = "Relative Attendance Rate")
+	})
+}
+
+shinyApp(ui = ui, server = server)
